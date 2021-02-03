63bd294c70194e5adc81b7a8d469fc1ad1f78853
nalhaarbi <nalhaarbi@ksu.edu.sa>
2021-02-03 09:02:13 +0300

    UPDATED

--- app/src/main/java/com/example/hwa/MainActivity.java
+++ app/src/main/java/com/example/hwa/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+// TEST
 public class MainActivity extends AppCompatActivity {
 
     Button b;
